8d581b697561e7692bb3b60bfef361eb1a509dc2
KieranVieira <kieranvieira@live.com>
2019-06-02 12:11:17 -0400

    removed pauseonhover from toasts

--- src/components/toasts/index.js
+++ src/components/toasts/index.js
@@ -3,7 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Toasts = () => (
-    <ToastContainer pauseOnHover={false}/>
+    <ToastContainer/>
 )
 
 export default Toasts;
